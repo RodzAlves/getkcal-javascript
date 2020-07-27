81395b0a79f45078aafae0232134d6d3f155177f
RodzAlves <rodrigoalvesbrasileiro@gmail.com>
2020-07-27 10:55:29 -0300

    Update verification

--- js/script.js
+++ js/script.js
@@ -12,41 +12,47 @@ function handleSubmit(event) {
     const height = getInputNumberValue('height');
     const activityLevel = getInputNumberValue('activity_level');
 
-    const tmb = Math.round((
+    if(!gender || !age || !weight || !height || !activityLevel) {
+      alert("Digite a informação que falta.");
+    } else {
+      const tmb = Math.round((
         gender === 'female'
         ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
         : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
-    ));
+      ));
 
-    const maintenance = Math.round((tmb * Number(activityLevel)));
-    const loseWeight = maintenance - 450;
-    const gainWeight = maintenance + 450;
+      const maintenance = Math.round((tmb * Number(activityLevel)));
+      const loseWeight = maintenance - 450;
+      const gainWeight = maintenance + 450;
 
-    const layout = `
-    <h2>Aqui está o resultado:</h2>
+      const layout = `
+      <h2>Aqui está o resultado:</h2>
 
-    <div class="result-content">
-      <ul>
-        <li>
-          Seu metabolismo basal é de <strong>${tmb} calorias</strong>.
-        </li>
-        <li>
-          Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>.
-        </li>
-        <li>
-          Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>.
-        </li>
-        <li>
-          Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>.
-        </li>
-      </ul>
-    </div>
-    `;
+      <div class="result-content">
+        <ul>
+          <li>
+            Seu metabolismo basal é de <strong>${tmb} calorias</strong>.
+          </li>
+          <li>
+            Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>.
+          </li>
+          <li>
+            Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>.
+          </li>
+          <li>
+            Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>.
+          </li>
+        </ul>
+      </div>
+      `;
 
-    const result = document.getElementById('result');
+      const result = document.getElementById('result');
 
-    result.innerHTML = layout;
-}
+      result.innerHTML = layout;
+    }
+    }
+
+    
 
 function getSelectedValue(id) {
     const select = document.getElementById(id);
